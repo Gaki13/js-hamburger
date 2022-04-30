e8f265d27091b4f4e6ab52d5163b9e3196bb555d
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-04-30 11:26:26 +0200

    aggiunte variabili di "base" per la selezione degli elementi

---
+++ main.js
@@ -0,0 +1,8 @@
+//1. Creo della variabili dove seleziono gli elementi che devo modificare
+const BtnOpen = document.getElementById("open-menu")
+
+const hamburgerMenu = document.getElementById("hamburger-menu")
+
+const BtnClose = document.getElementById("close-menu")
+//2. Aggiungo la funzione per aprire il menù al click
+//3. Aggiungo la funzione per chiudere il menù aò click
